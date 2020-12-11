66832e5f141674a5c566a40dd6732fd367d71927
Honza Blažek <gimli@matfyz.cz>
2020-12-11 14:20:55 +0100

    initcialization of the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tools for processing STL files in Python, comparison, transformations, etc.',
+    author='Honza Blažek',
+    license='',
+)
